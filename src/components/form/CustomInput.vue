dc6142c006ae71016d681838939f568bddeaa099
richard.liao <richard.liao@tpisoftware.com>
2024-09-19 09:17:56 +0800

    chore: reusable component input

---
+++ src/components/form/CustomInput.vue
@@ -0,0 +1,17 @@
+  <script>
+export default {
+  props: ["value"],
+  model: {
+    prop: "value",
+    event: "update",
+  },
+};
+</script>
+
+<template>
+  <input
+    type="text"
+    :value="value"
+    @input="$emit('update', $event.target.value)"
+  />
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <script lang="ts">
 import Vue from "vue";
 import TodoItem from "@/components/KKBOX.vue";
+import CustomInput from "@/components/form/CustomInput.vue";
 
 //使用 Vue.extend 才能加入 data 的型別
 export default Vue.extend({
-  components: { TodoItem },
+  components: { CustomInput },
+  data(): any {
+    return { email: "123" };
+  },
 });
 </script>
 
 <template>
-  <todo-item></todo-item>
+  <div>
+    <CustomInput id="email" labelText="Email" v-model="email" />
+    {{ email }}
+    <!-- <todo-item></todo-item> -->
+  </div>
 </template>
